ff1918f7e6cb03a55d690a38dbe864995f432c3c
Greg Cobb and Vinson Chuong <pair+gcobb+vchuong@pivotallabs.com>
2014-04-15 14:43:39 -0700

    [#69492216] Add search box and reposition page on submit

--- tasa/app/assets/javascripts/application.js
+++ tasa/app/assets/javascripts/application.js
@@ -6,6 +6,18 @@
 //= require templates/application
 (function() {
   'use strict';
+   var query = window.query = new Backbone.Model({query: ''});
 
   $('body').html(JST['templates/application']);
+
+  $('body').on('submit', '.query', function(e) {
+    _.each($(e.currentTarget).serializeArray(), function(input) {
+      query.set(input.name, input.value);
+    });
+    $(document.activeElement).blur();
+  });
+
+  query.on('change:query', function(query, value) {
+    $('body').toggleClass('has-query', Boolean(value));
+  });
 })();
--- tasa/spec/features/application_spec.rb
+++ tasa/spec/features/application_spec.rb
@@ -5,7 +5,12 @@ feature 'Application' do
     visit '/'
   end
 
-  scenario 'Bill searches for pokemon' do
+  scenario 'Bill searches for "Acrobatics"' do
     expect(page).to have_css('h1')
+
+    page.fill_in(:query, with: 'Acrobatics')
+    page.find('.query-input').native.send_keys(:return)
+
+    expect(page).to have_content('Tweets from July 1 - 31 of 2013')
   end
 end
