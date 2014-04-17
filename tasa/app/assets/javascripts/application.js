3f61a8f798cc342daa15176985a645fe6f330ddb
Greg Cobb and Vinson Chuong <pair+gcobb+vchuong@pivotallabs.com>
2014-04-17 14:49:02 -0700

    [finishes #69491510] Query box sticks to top of page. Add transition after querying. Allow re-querying.

--- tasa/app/assets/javascripts/application.js
+++ tasa/app/assets/javascripts/application.js
@@ -30,56 +30,77 @@
     $(document.activeElement).blur();
   });
 
+  $('body').one('webkitTransitionEnd', function() {
+    var
+      queryNode =  $('.query'),
+      threshold = queryNode.offset().top,
+      contentTop = $('.graphs').offset().top,
+      start = threshold,
+      finish = contentTop - queryNode.outerHeight() + 30
+      ;
+    $(window).scroll(function() {
+      var scrollTop = $('body').scrollTop(),
+        opacity = Math.min(Math.max((scrollTop - start) / (finish - start), 0), 0.99)
+        ;
+      queryNode.toggleClass('sticky',  scrollTop > threshold);
+      queryNode.css('background-color', queryNode.css('background-color').replace(/[\d.]+(?=\))/, opacity));
+    });
+  });
+
   query.on('change:query', function(query, value) {
     $('body').toggleClass('has-query', Boolean(value));
     totalTweets.fetch({reset: true});
   });
 
+  var graph;
   totalTweets.on('reset', function() {
-    var graph = new Rickshaw.Graph({
-      element: $('.total-tweets .graph')[0],
-      renderer: 'line',
-      height: 300,
-      series: [{data: totalTweets.toJSON(), color: '#fff'}]
-    });
+    if (!graph) {
+      graph = new Rickshaw.Graph({
+        element: $('.total-tweets .graph')[0],
+        renderer: 'line',
+        height: 300,
+        series: [{data: [{x: 0, y: 0}], color: '#fff'}]
+      });
 
-    new Rickshaw.Graph.Axis.Y({
-      element: $('.total-tweets .y-axis-left')[0],
-      graph: graph,
-      orientation: 'left',
-      width: 40,
-      height: 300,
-      tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-    });
+      new Rickshaw.Graph.Axis.Y({
+        element: $('.total-tweets .y-axis-left')[0],
+        graph: graph,
+        orientation: 'left',
+        width: 40,
+        height: 300,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+      });
 
-    new Rickshaw.Graph.Axis.Y({
-      element: $('.total-tweets .y-axis-right')[0],
-      graph: graph,
-      orientation: 'right',
-      width: 40,
-      height: 300,
-      tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-    });
+      new Rickshaw.Graph.Axis.Y({
+        element: $('.total-tweets .y-axis-right')[0],
+        graph: graph,
+        orientation: 'right',
+        width: 40,
+        height: 300,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+      });
 
-    new Rickshaw.Graph.Axis.X({
-      element: $('.total-tweets .x-axis')[0],
-      graph: graph,
-      orientation: 'bottom',
-      tickFormat: function(i) { return d3.time.format.utc('%b %e')(totalTweets.at(i).get('posted_date')); }
-    });
+      new Rickshaw.Graph.Axis.X({
+        element: $('.total-tweets .x-axis')[0],
+        graph: graph,
+        orientation: 'bottom',
+        tickFormat: function(i) { return d3.time.format.utc('%b %e')(totalTweets.at(i).get('posted_date')); }
+      });
 
-    new Rickshaw.Graph.HoverDetail({
-      graph: graph,
-      formatter: function(series, i, tweets) {
-        var point = totalTweets.at(i);
-        return '' +
-          '<span class="date">' + d3.time.format.utc('%b %e')(point.get('posted_date')) + '</span>' +
-          '<span class="tweets">' + point.get('num_tweets') + '</span>' +
-          '<span class="units">tweets</span>'
-        ;
-      }
-    });
+      new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        formatter: function(series, i, tweets) {
+          var point = totalTweets.at(i);
+          return '' +
+            '<span class="date">' + d3.time.format.utc('%b %e')(point.get('posted_date')) + '</span>' +
+            '<span class="tweets">' + point.get('num_tweets') + '</span>' +
+            '<span class="units">tweets</span>'
+          ;
+        }
+      });
+    }
 
+    graph.series[0].data = totalTweets.toJSON();
     graph.render();
   });
 })();
