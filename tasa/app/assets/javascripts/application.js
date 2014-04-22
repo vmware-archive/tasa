b39d6042729dc1b97d2e4d2630ce2242e6b0fd67
Greg Cobb and Vinson Chuong <pair+gcobb+vchuong@pivotallabs.com>
2014-04-21 17:17:06 -0700

    [finishes #69508328] Add sentiment time series graph

--- tasa/app/assets/javascripts/application.js
+++ tasa/app/assets/javascripts/application.js
@@ -7,23 +7,28 @@
 //= require d3.layout.cloud
 //= require_tree ./templates
 //= require views/spinner
+//= require views/time_series
 (function() {
   'use strict';
 
   var
-     query = new Backbone.Model({query: ''}),
-     totalTweets = new (Backbone.Collection.extend({
-       model: Backbone.Model.extend({
-         parse: function(response) { return {posted_date: new Date(response.posted_date), num_tweets: response.num_tweets} },
-         toJSON: function() { return {posted_date: this.get('posted_date'), y: this.get('num_tweets')}; }
-       }),
-       comparator: 'posted_date',
-       url: function() { return '/gp/topic/ts/q?sr_trm=' + query.get('query'); },
-       parse: function(response) { return response.tseries; },
-       toJSON: function() { return this.map(function(model, i) { return _.extend(model.toJSON(), {x: i}); }); }
-     }))(),
-     sideBarTweets = new Backbone.Collection(),
-     sideBar = new (Backbone.Model.extend({
+    query = new Backbone.Model({query: ''}),
+    totalTweets = new (Backbone.Collection.extend({
+      model: Backbone.Model.extend({
+        parse: function(response) { return {posted_date: new Date(response.posted_date), num_tweets: response.num_tweets} },
+        toJSON: function() { return {posted_date: this.get('posted_date'), y: this.get('num_tweets')}; }
+      }),
+      comparator: 'posted_date',
+      url: function() { return '/gp/topic/ts/q?sr_trm=' + query.get('query'); },
+      parse: function(response) { return response.tseries; },
+      toJSON: function() {
+        return [
+          {data: this.map(function(model, i) { return _.extend(model.toJSON(), {x: i}); }), color: '#fff'}
+        ];
+      }
+    }))(),
+    sideBarTweets = new Backbone.Collection(),
+    sideBar = new (Backbone.Model.extend({
        url: function() { return '/gp/tasa/relevant_tweets/?sr_trm='  + query.get('query'); },
        parse: function(attrs) {
          this.set('totalTweets', attrs.count);
@@ -35,7 +40,29 @@
          sideBarTweets.reset(attrs.tweets);
        }
      }))(),
-     adjectives = new (Backbone.Collection.extend({
+    sentiment = new (Backbone.Collection.extend({
+      model: Backbone.Model.extend({
+        parse: function(response) {
+          return {
+            posted_date: new Date(response.posted_date),
+            negative_count: response.negative_count,
+            positive_count: response.positive_count,
+            neutral_count: response.neutral_count
+          }
+        }
+      }),
+      comparator: 'posted_date',
+      url: function() { return '/gp/senti/ms/q?sr_trm=' + query.get('query'); },
+      parse: function(response) { return response.multi_series; },
+      toJSON: function() {
+        return [
+          {data: this.map(function(model, i){ return {posted_date: model.get('posted_date'), x: i, y: model.get('positive_count')}}), color: '#d9e021'},
+          {data: this.map(function(model, i){ return {posted_date: model.get('posted_date'), x: i, y: model.get('neutral_count')}}), color: 'rgba(255, 255, 255, 0.3)'},
+          {data: this.map(function(model, i){ return {posted_date: model.get('posted_date'), x: i, y: model.get('negative_count')}}), color: '#e74d00'}
+        ];
+      }
+    }))(),
+    adjectives = new (Backbone.Collection.extend({
        url: function() { return '/gp/senti/acloud/q?sr_trm=' + query.get('query'); },
        parse: function(response) { return response.adjective_cloud; },
        comparator: 'normalized_frequency'
@@ -52,60 +79,13 @@
       return {tweets: sideBarTweets.toJSON(), totalTweets: sideBar.get('totalTweets')};
     }
   });
-  var totalTweetsView = new SpinnerView({
+  var totalTweetsView = new TimeSeriesView({
     el: $('.total-tweets .graph-content'),
-    template: 'templates/total_tweets',
-    model: totalTweets,
-    render: function(options) {
-      SpinnerView.prototype.render.call(this, options);
-      if (options.loading) { return; }
-
-      var graph = new Rickshaw.Graph({
-        element: $('.total-tweets .graph')[0],
-        renderer: 'line',
-        height: 300,
-        series: [{data: totalTweets.toJSON(), color: '#fff'}]
-      });
-
-      new Rickshaw.Graph.Axis.Y({
-        element: $('.total-tweets .y-axis-left')[0],
-        graph: graph,
-        orientation: 'left',
-        width: 40,
-        height: 300,
-        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-      });
-
-      new Rickshaw.Graph.Axis.Y({
-        element: $('.total-tweets .y-axis-right')[0],
-        graph: graph,
-        orientation: 'right',
-        width: 40,
-        height: 300,
-        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-      });
-
-      new Rickshaw.Graph.Axis.X({
-        element: $('.total-tweets .x-axis')[0],
-        graph: graph,
-        orientation: 'bottom',
-        tickFormat: function(i) { return d3.time.format.utc('%B %e')(totalTweets.at(i).get('posted_date')); }
-      });
-
-      new Rickshaw.Graph.HoverDetail({
-        graph: graph,
-        formatter: function(series, i, tweets) {
-          var point = totalTweets.at(i);
-          return '' +
-            '<span class="date">' + d3.time.format.utc('%B %e')(point.get('posted_date')) + '</span>' +
-            '<span class="tweets">' + point.get('num_tweets') + '</span>' +
-            '<span class="units">tweets</span>'
-            ;
-        }
-      });
-
-      graph.render();
-    }
+    model: totalTweets
+  });
+  var sentimentView = new TimeSeriesView({
+    el: $('.sentiment .graph-content'),
+    model: sentiment
   });
   var adjectivesView = new SpinnerView({
     el: $('.adjectives .tag-cloud'),
@@ -182,6 +162,6 @@
 
   query.on('change:query', function(query, value) {
     $('body').toggleClass('has-query', Boolean(value));
-    _.invoke([totalTweets, sideBar, adjectives], 'fetch', {reset: true});
+    _.invoke([totalTweets, sideBar, sentiment, adjectives], 'fetch', {reset: true});
   });
 })();
---
+++ tasa/app/assets/javascripts/views/time_series.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict';
+  window.TimeSeriesView = SpinnerView.extend({
+    template: 'templates/time_series',
+
+    render: function(options) {
+      var self = this;
+      SpinnerView.prototype.render.call(this, options);
+      if (options.loading) { return; }
+
+      var graph = new Rickshaw.Graph({
+        element: this.$('.graph')[0],
+        renderer: 'line',
+        height: 300,
+        series: this.model.toJSON()
+      });
+
+      new Rickshaw.Graph.Axis.Y({
+        element: this.$('.y-axis-left')[0],
+        graph: graph,
+        orientation: 'left',
+        width: 40,
+        height: 300,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+      });
+
+      new Rickshaw.Graph.Axis.Y({
+        element: this.$('.y-axis-right')[0],
+        graph: graph,
+        orientation: 'right',
+        width: 40,
+        height: 300,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+      });
+
+      new Rickshaw.Graph.Axis.X({
+        element: this.$('.x-axis')[0],
+        graph: graph,
+        orientation: 'bottom',
+        tickFormat: function(i) { return d3.time.format.utc('%B %e')(self.model.at(i).get('posted_date')); }
+      });
+
+      new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        formatter: function(series, i, tweets) {
+          return '' +
+            '<div style="color:' + series.color + '">' +
+              '<span class="date">' + d3.time.format.utc('%B %e')(self.model.at(i).get('posted_date')) + '</span>' +
+              '<span class="tweets">' + series.data[i].y + '</span>' +
+              '<span class="units">tweets</span>' +
+            '</div>'
+            ;
+        }
+      });
+
+      graph.render();
+    }
+  });
+})();
