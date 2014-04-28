12299aa88c999a36fdf6b7275661b65309ddea7f
Greg Cobb and Vinson Chuong <pair+gcobb+vchuong@pivotallabs.com>
2014-04-28 15:55:23 -0700

    [#70325460] Remove ActiveRecord

--- tasa/spec/spec_helper.rb
+++ tasa/spec/spec_helper.rb
@@ -14,17 +14,5 @@ Capybara.default_wait_time = 60
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-  end
-
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.use_transactional_fixtures = false
-  config.before { DatabaseCleaner.strategy = :transaction }
-  config.before(type: feature) { DatabaseCleaner.strategy = :truncation }
-  config.before { DatabaseCleaner.start }
-  config.after { DatabaseCleaner.clean }
-
   config.order = 'random'
 end
