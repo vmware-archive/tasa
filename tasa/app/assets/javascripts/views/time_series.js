b60b03058c053a9d23ba69220b0ac2c6b99f61ce
Greg Cobb and Vinson Chuong <pair+gcobb+vchuong@pivotallabs.com>
2014-04-28 10:33:07 -0700

    [finishes #69491756] Add weekend markers to time series graphs

--- tasa/app/assets/javascripts/views/time_series.js
+++ tasa/app/assets/javascripts/views/time_series.js
@@ -21,6 +21,34 @@
         max: max * 1.1
       });
 
+      graph.onUpdate(function() {
+        d3.select(self.$('.graph svg').selector)
+          .selectAll('rect.weekend')
+          .data(_.select(series[0].data, function(point) {
+            return _.contains(['Sat', 'Sun'], d3.time.format.utc('%a')(point.posted_date));
+          }))
+          .enter()
+            .insert('g', 'svg > path:first-of-type')
+              .attr('class', 'weekend')
+              .attr('transform', function(d) { return 'translate(' + graph.x(d.x)  + ', 0)'; })
+              .call(function(g) {
+                g.each(function(d, i) {
+                  var container = d3.select(g[0][i]);
+                  container.append('text')
+                    .attr('dx', function(d) { return (graph.x(d.x + 1) - graph.x(d.x)) / 2; })
+                    .attr('dy', '1em')
+                    .attr('text-anchor', 'middle')
+                    .text(function(d) { return d3.time.format.utc('%a')(d.posted_date); });
+                  container.append('rect')
+                    .attr('width', function(d) { return graph.x(d.x + 1) - graph.x(d.x); })
+                    .attr('height', graph.y.range()[0] - graph.y.range()[1])
+                  ;
+                });
+              })
+        ;
+      });
+
+
       new Rickshaw.Graph.Axis.Y({
         element: this.$('.y-axis-left')[0],
         graph: graph,
